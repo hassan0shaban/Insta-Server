ec78ccb212a6a55a69051c1fc6aad362b696fb78
Harera <61611892+hassan0shaban@users.noreply.github.com>
2021-10-23 22:38:43 +0200

    update user table

--- src/main/kotlin/com/example/dp/table/UserTable.kt
+++ src/main/kotlin/com/example/dp/table/UserTable.kt
@@ -6,10 +6,12 @@ import org.jetbrains.exposed.sql.Table
 
 object UserTable : Table(TableNames.User) {
     val uid = integer(UserFields.uid).primaryKey().autoIncrement()
-    val userName = varchar(UserFields.username, 80).primaryKey().default(uid.toString())
+    val email = varchar(UserFields.email, 120).primaryKey()
+    val userName = varchar(UserFields.username, 80).primaryKey().default("null")
+    val password = varchar(UserFields.password, 50)
     val phoneNumber = varchar(UserFields.phone_number, 45).nullable()
     val user_image_url = varchar(UserFields.user_image_url, 300).nullable()
-    val name = varchar(UserFields.name, 45)
-    val email = varchar(UserFields.email, 120).nullable()
+    val name = varchar(UserFields.name, 45).nullable()
+    val token = varchar(UserFields.token, 3000).nullable()
     val bio = varchar(UserFields.bio, 300).nullable()
 }
