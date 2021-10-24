673bf1abcd06221ab4ca869b7c9666eece1737ef
Harera <61611892+hassan0shaban@users.noreply.github.com>
2021-10-24 21:29:36 +0200

    setup auth with jwt

--- src/main/kotlin/com/example/Application.kt
+++ src/main/kotlin/com/example/Application.kt
@@ -1,12 +1,18 @@
 package com.example
 
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
 import com.example.di.mainModule
+import com.example.route.feedRouting
+import com.example.route.loginRouting
+import com.example.route.signupRouting
 import com.example.route.userRouting
 import io.ktor.application.*
+import io.ktor.auth.*
+import io.ktor.auth.jwt.*
 import io.ktor.features.*
 import io.ktor.gson.*
 import io.ktor.routing.*
-import org.jetbrains.exposed.sql.Database
 import org.koin.ktor.ext.Koin
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
@@ -15,7 +21,34 @@ fun Application.module() {
     install(ContentNegotiation) { gson { } }
     install(Koin) { modules(mainModule) }
 
+    val secret = environment.config.property("jwt.secret").getString()
+    val issuer = environment.config.property("jwt.issuer").getString()
+    val audience = environment.config.property("jwt.audience").getString()
+    val myRealm = environment.config.property("jwt.realm").getString()
+
+    install(Authentication) {
+        jwt("auth-jwt") {
+            realm = myRealm
+            verifier(
+                JWT.require(Algorithm.HMAC256(secret))
+                    .withAudience(audience)
+                    .withIssuer(issuer)
+                    .build()
+            )
+
+            validate { credential ->
+                if (credential.payload.getClaim("email").asString() != "") {
+                    JWTPrincipal(credential.payload)
+                } else {
+                    null
+                }
+            }
+        }
+    }
     routing {
         userRouting()
+        signupRouting()
+        loginRouting(secret = secret, issuer = issuer, audience = audience)
+        feedRouting(secret = secret, issuer = issuer, audience = audience)
     }
 }
